116d63d02b0e3c5151f3e38d402f3b5e269222bf
Moises0011 <moisesnascfernandes@gmail.com>
2023-03-26 08:34:15 -0400

    ajustes

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { TextInput, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (inputText) => {
+    setEmail(inputText);
+  };
+
+  const handlePasswordChange = (inputText) => {
+    setPassword(inputText);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.title}>
+        Login
+      </Text>
+
+      <TextInput
+        style={styles.textInput}
+        placeholder="Digite seu e-mail aqui"
+        onChangeText={handleEmailChange}
+        value={email}
+      />
+
+      <TextInput
+        style={styles.textInput}
+        placeholder="Digite sua senha aqui"
+        secureTextEntry={true}
+        onChangeText={handlePasswordChange}
+        value={password}
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    margin: 10,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 15,
+  },
+  button:{
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
+
+export default Login;
